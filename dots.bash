57e66d14aad717a579d4f2913446c0ab02194581
freddy <freddy@6dcc17ee-e80d-0410-aa1b-d7ec13c31852>
2006-03-26 22:16:52 +0000

    Added tests for without argument

--- dots.bash
+++ dots.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 # --- dots.bash -----------------------------------------------------------
-# Change directory back - down the directory tree - 1-7 times.
+# Change directory back - up the directory tree - 1-7 times.
 # Usage: ..[.[.[.[.[.[.]]]]]] [dir]
 #
-# Arguments: [dir]   Directory to go forth - up the directory tree again,
+# Arguments: [dir]   Directory to go forth - down the directory tree again,
 #                    with TAB-completion!
 #
 # Example:   $/usr/share> .. local/share/   # .. lo[TAB]/sh[TAB])
@@ -12,32 +12,31 @@
 # Install:   Add the code underneath to your ~/.bashrc
 
 
-    # TAB completion for the .., ..., ...., etc. commands
+    # TAB completion for the .. ... .... etc commands
 _completeDots() {
     local argToComplete prevArg
 
     COMPREPLY=()
     argToComplete=${COMP_WORDS[COMP_CWORD]}
-    dots=${COMP_WORDS[COMP_CWORD-1]:1}  # ':1' = Ignore first dot at pos 0
-	cd ${dots//./..\/}                  # Replace . with ../, and cd
-	COMPREPLY=( $(
-		compgen -S '/' -d -- "${argToComplete}"
-	) )
-	cd -  # cd to prev dir
+    dots=${COMP_WORDS[COMP_CWORD-1]:2}  # ':2' = Ignore two dots at pos 0
+	cd ${dots//./..\/}..                # Replace . with ../, and cd
+	COMPREPLY=( $(compgen -S '/' -d -- "${argToComplete}") )
+	cd - > /dev/null                    # Restore current dir
 } # _completeDots
 
 
-	# Define functions for .., ..., ...., etc. and associate _completeDots() with 'em
+	# Define functions .. ... .... etc
 dotsFunctions=
 for ((i = 1; i <= 7; i++)); do
 	dots=.; dotsPath=
 	for ((j = 1; j <= $i; j++)); do
-		dots=$dots.; dotsPath=$path../
+		dots=$dots.; dotsPath=$dotsPath../
 	done	
-		# Make sure .., ..., ...., etc. is unaliased
+		# Make sure .. ... .... etc is unaliased
 	alias $dots &> /dev/null && unalias $dots
-		# Define functions for .., ..., ...., etc.
+		# Function definition
 	eval "$dots() { if [ $dotsPath\$1 ]; then cd $dotsPath\$1; fi }"
 	dotsFunctions="$dotsFunctions $dots"
 done
+	# Link functions .. ... .... etc with '_completeDots()'.
 complete -o nospace -F _completeDots $dotsFunctions
