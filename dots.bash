48806de24883dc15d379dd29ea55515e20c134c9
freddy <freddy@6dcc17ee-e80d-0410-aa1b-d7ec13c31852>
2006-03-26 17:01:26 +0000

    Shortened dots.bash

--- dots.bash
+++ dots.bash
@@ -1,93 +1,43 @@
 #!/usr/bin/bash
-# --- dots.bash ---
-# Change directory back - up the directory tree - 1-7 times.
+# --- dots.bash -----------------------------------------------------------
+# Change directory back - down the directory tree - 1-7 times.
 # Usage: ..[.[.[.[.[.[.]]]]]] [dir]
 #
-# Arguments:
-# 
-#    [dir]   Directory to go forth - down the directory tree again,
-#            with TAB-completion!
+# Arguments: [dir]   Directory to go forth - up the directory tree again,
+#                    with TAB-completion!
 #
-# Example:
+# Example:   $/usr/share> .. local/share/   # .. lo[TAB]/sh[TAB])
+#            $/usr/local/share>  
 #
-#    $/usr/share> .. local/share/   # .. lo[TAB]/sh[TAB])
-#    $/usr/local/share>  
-#
-# Install: Save this file as ~/dots.bash and add the line underneath to ~/.bashrc:
-#
-#    source ~/dots.bash
-#
+# Install:   Add the code underneath to your ~/.bashrc
 
 
-	# If aliases exist for the 'change directory 1-7 up' commands, unalias them
-for nup in .. ... .... ..... ...... ....... ........; do
-	if alias $nup &> /dev/null; then
-		unalias $nup
-	fi
-done
-
-
-# Change directory 1 up
-function ..() {
-	if [ ../$1 ]; then cd ../$1; fi
-} # ..()
-
-# Change directory 2 up
-function ...() {
-	if [ ../../$1 ]; then cd ../../$1; fi
-} # ...()
-
-# Change directory 3 up
-function ....() {
-	if [ ../../../$1 ]; then cd ../../../$1; fi
-} # ....()
-
-# Change directory 4 up
-function .....() {
-	if [ ../../../../$1 ]; then cd $1; fi
-} # .....()
-
-# Change directory 5 up
-function ......() {
-	if [ ../../../../../$1 ]; then cd $1; fi
-} # .....()
-
-# Change directory 6 up
-function .......() {
-	if [ ../../../../../../$1 ]; then cd $1; fi
-} # .....()
-
-# Change directory 7 up
-function ........() {
-	if [ ../../../../../../../$1 ]; then cd $1; fi
-} # .....()
-
-
-    # TAB completion for the 'change directory n up' commands
-_..() {
-    local arg_to_complete prev_arg
+    # TAB completion for the .., ..., ...., etc. commands
+_completeDots() {
+    local argToComplete prevArg
 
     COMPREPLY=()
-    arg_to_complete=${COMP_WORDS[COMP_CWORD]}
-    prev_arg=${COMP_WORDS[COMP_CWORD-1]}
-    case $prev_arg in
-        '..')       cd_dir=..                   ;;
-        '...')      cd_dir=../..                ;;
-        '....')     cd_dir=../../..             ;;
-        '.....')    cd_dir=../../../..          ;;
-        '......')   cd_dir=../../../../..       ;;
-        '.......')  cd_dir=../../../../../..    ;;
-        '........') cd_dir=../../../../../../.. ;;
-    esac
-	if [ $cd_dir ]; then
-		prev_dir=`pwd`
-		cd $cd_dir
-		COMPREPLY=( $(
-			compgen -S '/' -d -- "${arg_to_complete}"
-		) )
-		cd $prev_dir
-	fi
+    argToComplete=${COMP_WORDS[COMP_CWORD]}
+    dots=${COMP_WORDS[COMP_CWORD-1]:1}  # ':1' = Ignore first dot at pos 0
+	cd ${dots//./..\/}                  # Replace . with ../, and cd
+	COMPREPLY=( $(
+		compgen -S '/' -d -- "${argToComplete}"
+	) )
+	cd -  # cd to prev dir
+} # _completeDots
 
-    return 0
-} # _cdp()
-complete -o nospace -F _.. .. ... .... ..... ...... ....... ........
+
+	# Define functions for .., ..., ...., etc. and associate _completeDots() with 'em
+dotsFunctions=
+for ((i = 1; i <= 7; i++)); do
+	dots=.; dotsPath=
+	for ((j = 1; j <= $i; j++)); do
+		dots=$dots.; dotsPath=$path../
+	done	
+		# Make sure .., ..., ...., etc. is unaliased
+	alias $dots &> /dev/null && unalias $dots
+		# Define functions for .., ..., ...., etc.
+	eval "$dots() { if [ $dotsPath\$1 ]; then cd $dotsPath\$1; fi }"
+	dotsFunctions="$dotsFunctions $dots"
+done
+complete -o nospace -F _completeDots $dotsFunctions
