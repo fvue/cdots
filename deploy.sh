c455b3ac061cb18ddd05942e2ad825c89139d638
freddy <freddy@6dcc17ee-e80d-0410-aa1b-d7ec13c31852>
2006-09-24 20:13:40 +0000

    Added support for whitespace and special characters in filenames.  1 Test to go.

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+pversion=bashdots-$(cat VERSION)
+ln -s . $pversion
+
 	# @NOTE: Do not exclude all .svn directories since the test suite
     #        expects some .svn directories in the fixture directories
-tar -cvzf bashdots-$(cat VERSION).tar.gz \
-	--exclude=.svn/* \
-	--exclude=log/* \
+tar -cvzf $pversion.tar.gz \
+	--exclude='.svn/*' \
+	--exclude='log/*' \
 	--exclude=test/.svn \
 	--exclude=test/all/.svn \
 	--exclude='*.swp' \
-	dots.sh dots-completion.sh dots-functions.sh VERSION RELEASE test
+	${pversion}/dots.sh ${pversion}/dots-completion.sh ${pversion}/dots-functions.sh ${pversion}/AUTHORS ${pversion}/VERSION ${pversion}/RELEASE ${pversion}/test
 
-scp bashdots-$(cat VERSION).tar.gz fvulto@www.fvue.nl:~/www/www.fvue.nl/bashdots/download/
+    # Remove temporary directory
+[ -h $pversion ] && rm $pversion
+
+    # Link bashdots-1.0.current.tar.gz
+[ -f bashdots-1.0.current.tar.gz ] && rm bashdots-1.0.current.tar.gz
+ln -s $pversion.tar.gz bashdots-1.0.current.tar.gz
+
+    # Copy distribution to live server
+scp $pversion.tar.gz fvulto@www.fvue.nl:~/www/www.fvue.nl/bashdots/download/
--- dots.sh
+++ dots.sh
@@ -17,22 +17,33 @@ DOTS_DEPTH=7
 
 
     # TAB completion for the .. ... .... etc commands
-_completeDots() {
+_cdots() {
     local dots=${COMP_WORDS[COMP_CWORD-1]:2}  # ':2' = Ignore two dots at pos 0
-	COMPREPLY=( $(
-		cd ${dots//./..\/}.. > /dev/null
-		compgen -o nospace -S '/' -d -- "${COMP_WORDS[COMP_CWORD]}"
-	) )
+    local IFS=$'\012'  # newline
+    local i j k cur=${COMP_WORDS[COMP_CWORD]}
+		#if [ -d ${dots//./..\/}.. ]; then
+        #fi
+    k=${#COMPREPLY[@]};
+    for j in $( 
+        cd ${dots//./..\/}.. > /dev/null
+        compgen -d -- "${COMP_WORDS[COMP_CWORD]}"
+    ); do
+            # If j not directory in current working directory, append slash '/'
+            # NOTE: If j also directory in current working directory, 
+            #       'complete -o filenames' automatically appends slash '/'
+        [ ! -d $j ] && j="$j/"
+        COMPREPLY[k++]="$j"
+    done
 } # _completeDots
 
 
 # Change directory to specified directory at [level] directories back
 # @param $1 integer  Level
 # @param $2 string   Directory
-function dots() {
+function cdots() {
     local i dir
-    for ((i = 0; i < $1; i++)); do dir=../$dir; done
-    cd $dir$2
+    for ((i = 0; i < $1; i++)); do dir="../$dir"; done
+    cd "$dir$2"
 } # dots()
 
 
@@ -43,9 +54,10 @@ dotsAliases=
 for ((i = 1; i <= $DOTS_DEPTH; i++)); do
 	dots=.
 	for ((j = 1; j <= $i; j++)); do dots=$dots.; done	
-	alias $dots="dots $i"
+	alias $dots="cdots $i"
 	dotsAliases="$dotsAliases $dots"
 done
-	# Link aliases .. ... .... etc with '_completeDots()'.
-complete -o nospace -F _completeDots $dotsAliases
+	# Link aliases .. ... .... etc with '_cdots()'.
+    # Options: -o filenames: Escapes whitespace
+complete -o filenames -o nospace -F _cdots $dotsAliases
 unset -v DOTS_DEPTH dots dotsAliases i j
